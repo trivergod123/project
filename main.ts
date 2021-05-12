9e577ed71903d2fb9551af8b318d225f91be0233
trivergod123 <82467786+trivergod123@users.noreply.github.com>
2021-05-12 13:59:44 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . 
+    . . . f f f f f f . . . . 
+    . f f f f f f f f f . . . 
+    . f f f f f f c f f f . . 
+    f f f f c f f f c f f f . 
+    f c f f c c f f f c c f f 
+    f c c f f f f e f f f f f 
+    f f f f f f f e e f f f . 
+    f f e e f b f e e f f f . 
+    f f e 4 e 1 f 4 4 f f . . 
+    . f f f e 4 4 4 4 f . . . 
+    . 4 4 4 e e e e f f . . . 
+    . e 4 4 e 7 7 7 7 f . . . 
+    . f e e f 6 6 6 6 f f . . 
+    . f f f f f f f f f f . . 
+    . . f f . . . f f f . . . 
+    `, SpriteKind.Player)
+scene.setBackgroundColor(1)
